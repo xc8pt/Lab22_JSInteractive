764c5f5330ffb785d2ab82d18849e58bd6bd51ad
duxa4854 <nzaichenko531@gmail.com>
2026-04-01 10:50:19 +0300

    Step 4: added basic JS code for calculator

---
+++ calculator/main.js
@@ -0,0 +1,28 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear");
+
+let expression = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+    if (value === "C") {
+      expression = "";
+      display.value = "";
+      return;
+    }
+    if (value === "=") {
+      try {
+        expression = eval(expression);
+        display.value = expression;
+      } catch {
+        display.value = "Ошибка";
+        expression = "";
+      }
+      return;
+    }
+    expression += value;
+    display.value = expression;
+  });
+});
